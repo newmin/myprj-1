708a377cf4b4f3070afaa50dc33ec07400921308
sanghak <sang105kr@gmail.com>
2021-08-19 14:49:09 +0900

    초기화면 구현 및 로그인

--- src/main/java/com/kh/myprj/web/HomeController.java
+++ src/main/java/com/kh/myprj/web/HomeController.java
@@ -1,13 +1,54 @@
 package com.kh.myprj.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import javax.validation.Valid;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.kh.myprj.web.form.LoginForm;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 
+	//초기화면
 	@GetMapping("/")
 	public String home() {
+		return "home";
+	}
+		
+	//로그인 양식
+	@GetMapping("/login")
+	public String loginForm(@ModelAttribute LoginForm loginForm) {
+		return "loginForm";
+	}
+	
+	//로그인 처리
+	@PostMapping("/login")
+	public String login(
+			@Valid @ModelAttribute LoginForm loginForm, 
+			BindingResult bindingResult) {
+		
+		log.info("LoginForm:{}",loginForm);
+		
+		if(bindingResult.hasErrors()) {
+			log.info("BindingResult:{}",bindingResult);
+			return "loginForm";
+		}
+		
+		return "home";
+	}
+	
+	//로그아웃
+	@GetMapping("/logout")
+	public String logout() {
+		//세션제거
 		return "home";
 	}
 }
---
+++ src/main/java/com/kh/myprj/web/form/LoginForm.java
@@ -0,0 +1,21 @@
+package com.kh.myprj.web.form;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+	
+	@NotBlank
+	@Email
+	@Size(min = 4,max = 40)
+	private String email;
+	
+	@NotBlank
+	@Size(min = 4,max = 12)
+	private String pw;
+	
+}
