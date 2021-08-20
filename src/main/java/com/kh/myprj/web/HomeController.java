f0f98d18525ed23b0e75313566ab234afd81c9b9
sanghak <sang105kr@gmail.com>
2021-08-20 09:11:11 +0900

    메시제처리반영

--- src/main/java/com/kh/myprj/web/HomeController.java
+++ src/main/java/com/kh/myprj/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.kh.myprj.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.kh.myprj.web.form.LoginForm;
+import com.kh.myprj.web.form.LoginMember;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -33,16 +36,31 @@ public class HomeController {
 	@PostMapping("/login")
 	public String login(
 			@Valid @ModelAttribute LoginForm loginForm, 
-			BindingResult bindingResult) {
+			BindingResult bindingResult,
+			Model model, HttpServletRequest request) {
 		
 		log.info("LoginForm:{}",loginForm);
 		
+		LoginMember loginMember = null;		
+		//계정확인
+		if("user@test.com".equals(loginForm.getEmail()) && "user1234".equals(loginForm.getPw())) {
+			loginMember = new LoginMember("user","회원",false);
+		}else if("admin@test.com".equals(loginForm.getEmail()) && "admin1234".equals(loginForm.getPw())){
+			loginMember = new LoginMember("admin","관리자",true);
+		}else {
+			bindingResult.reject("loginChk", "아이디 또는 비밀번호가 잘못되었습니다");			
+		}
+		//글로벌오류 체크
 		if(bindingResult.hasErrors()) {
 			log.info("BindingResult:{}",bindingResult);
 			return "loginForm";
 		}
 		
-		return "home";
+		//세션생성
+		HttpSession session =request.getSession(true);
+		session.setAttribute("loginMember", loginMember );
+		
+		return "redirect:/";
 	}
 	
 	//로그아웃
